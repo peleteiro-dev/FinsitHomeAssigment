3bff9f6ac65855c16edda5321bc069430e846da0
peleteiro_dev <peleteiro.dev@gmail.com>
2022-01-24 22:11:04 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace FinsitHomeAssigment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}

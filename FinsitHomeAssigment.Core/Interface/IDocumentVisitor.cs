eade18d929bee561f497fccc558a79aaa52f43dc
peleteiro_dev <peleteiro.dev@gmail.com>
2022-01-24 23:21:26 +0100

    add methods to interface and create html visitor

--- FinsitHomeAssigment.Core/Interface/IDocumentVisitor.cs
+++ FinsitHomeAssigment.Core/Interface/IDocumentVisitor.cs
@@ -1,6 +1,13 @@
-﻿namespace FinsitHomeAssigment.Core.Interface
+﻿using FinsitHomeAssigment.Core.Model;
+
+namespace FinsitHomeAssigment.Core.Interface
 {
     public interface IDocumentVisitor
     {
+        void Visit(Document document);
+        void Visit(Section section);
+        void Visit(Paragraph paragraph);
+        void Visit(Text text);
+        void Visit(BoldText boldText);
     }
 }
---
+++ FinsitHomeAssigment.Core/Visitor/HtmlVisitor.cs
@@ -0,0 +1,34 @@
+﻿using FinsitHomeAssigment.Core.Interface;
+using FinsitHomeAssigment.Core.Model;
+using System;
+
+namespace FinsitHomeAssigment.Core.Visitor
+{
+    public class HtmlVisitor : IDocumentVisitor
+    {
+        public void Visit(Document document)
+        {
+            Console.WriteLine($"{document.GetType().Name} visited by {this.GetType().Name}");
+        }
+
+        public void Visit(Section section)
+        {
+            Console.WriteLine($"{section.GetType().Name} visited by {this.GetType().Name}");
+        }
+
+        public void Visit(Paragraph paragraph)
+        {
+            Console.WriteLine($"{paragraph.GetType().Name} visited by {this.GetType().Name}");
+        }
+
+        public void Visit(Text text)
+        {
+            Console.WriteLine($"{text.GetType().Name} visited by {this.GetType().Name}");
+        }
+
+        public void Visit(BoldText boldText)
+        {
+            Console.WriteLine($"{boldText.GetType().Name} visited by {this.GetType().Name}");
+        }
+    }
+}

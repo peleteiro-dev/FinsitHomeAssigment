e5935460a9879c1c79ae8a369f265ae795ae5d7f
peleteiro_dev <peleteiro.dev@gmail.com>
2022-02-18 21:19:04 +0100

    reduce markdown parser tests data dependency

--- FinsitHomeAssigment.Core.UnitTests/Parser/MarkdownParserTests.cs
+++ FinsitHomeAssigment.Core.UnitTests/Parser/MarkdownParserTests.cs
@@ -11,19 +11,12 @@ namespace FinsitHomeAssigment.Core.UnitTests.Parser
         private IEnumerable<string> _textToParse;
         private MarkdownParser _markdownParser;
         private Document _expectedOutput;
-        private Section _section1;
-        private Section _section2;
-        private SubSection _subSection1;
+        private Section _section;
+        private SubSection _subSection;
         private SubSection _nestedSubsection;
-        private Paragraph _paragraph1;
-        private Paragraph _paragraph2;
-        private Paragraph _paragraph3;
-        private Paragraph _subSectionParagraph;
-        private Paragraph _section2Paragraph1;
-        private Paragraph _section2Paragraph2;
-        private Paragraph _nestedSubSectionParagraph;
-        private Paragraph _oneLineParagraph;
-        private Paragraph _multiLineParagraph;
+        private Paragraph _paragraph;
+        private readonly Text _text = new Text("text");
+        private readonly BoldText _boldText = new BoldText("bold text");
 
         private void Setup(string testCaseFile)
         {
@@ -34,134 +27,120 @@ namespace FinsitHomeAssigment.Core.UnitTests.Parser
             _markdownParser = new MarkdownParser();
 
             _expectedOutput = new Document();
-            _section1 = new Section("Section 1");
-            _subSection1 = new SubSection("Subsection 1.1");
-            _nestedSubsection = new SubSection("Nested Subsection 1.1.1");
-            _section2 = new Section("Section 2");
-
-            _paragraph1 = new Paragraph();
-            _paragraph1.AddDocumentElement(new Text("Some "));
-            _paragraph1.AddDocumentElement(new BoldText("(bold) introduction"));
-            _paragraph1.AddDocumentElement(new Text(" to Section 1."));
-
-            _paragraph2 = new Paragraph();
-            _paragraph2.AddDocumentElement(new Text("A text describing Section 1.1"));
-
-            _paragraph3 = new Paragraph();
-            _paragraph3.AddDocumentElement(new Text("Some conclusion to Section 1."));
-
-            _subSectionParagraph = new Paragraph();
-            _subSectionParagraph.AddDocumentElement(new Text("An introduction to Subsection 1.1."));
-
-            _nestedSubSectionParagraph = new Paragraph();
-            _nestedSubSectionParagraph.AddDocumentElement(new Text("An introduction to Nested Subsection 1.1.1."));
-
-            _section2Paragraph1 = new Paragraph();
-            _section2Paragraph1.AddDocumentElement(new Text("An introduction to Section 2."));
-
-            _section2Paragraph2 = new Paragraph();
-            _section2Paragraph2.AddDocumentElement(new Text("Some conclusion to Section 2."));
-
-            _oneLineParagraph = new Paragraph();
-            _oneLineParagraph.AddDocumentElement(new Text("An introduction to Section 1."));
-
-            _multiLineParagraph = new Paragraph();
-            _multiLineParagraph.AddDocumentElement(new Text("Some text "));
-            _multiLineParagraph.AddDocumentElement(new BoldText("introduction"));
-            _multiLineParagraph.AddDocumentElement(new Text(" to "));
-            _multiLineParagraph.AddDocumentElement(new BoldText("this paragraph."));
-            _multiLineParagraph.AddDocumentElement(new Text("A second line in the same paragraph."));
-            _multiLineParagraph.AddDocumentElement(new BoldText("And a third bold line."));
+            _section = new Section("section");
+            _subSection = new SubSection("subsection");
+            _nestedSubsection = new SubSection("nested subsection");
+            _paragraph = new Paragraph();
         }
 
         [Fact]
-        public void RequiredCase()
-        {
-            const string testCaseFile = "RequiredText.txt";
-            Setup(testCaseFile);
-
-            var parsedText = _markdownParser.Parse(_textToParse);
-
-            _expectedOutput.AddDocumentElement(_section1);
-            _section1.AddDocumentElement(_paragraph1);
-            _section1.AddDocumentElement(_subSection1);
-            _subSection1.AddDocumentElement(_paragraph2);
-            _subSection1.AddDocumentElement(_paragraph3);
-            _expectedOutput.AddDocumentElement(_section2);
-            _section2.AddDocumentElement(_section2Paragraph1);
-            _section2.AddDocumentElement(_section2Paragraph2);
-
-            var equal = _expectedOutput.Equals(parsedText);
-            Assert.True(equal);
-        }
-
-        [Fact]
-        public void OneSectionAndOneLineParagraph()
-        {
-            const string testCaseFile = "OneSectionAndOneLineParagraph.txt";
-            Setup(testCaseFile);
-
-            var parsedText = _markdownParser.Parse(_textToParse);
-
-            _expectedOutput.AddDocumentElement(_section1);
-            _section1.AddDocumentElement(_oneLineParagraph);
-            var equal = _expectedOutput.Equals(parsedText);
-            Assert.True(equal);
-        }
-
-        [Fact]
-        public void OnlyOneMultilineParagraph()
-        {
-            const string testCaseFile = "OnlyOneMultilineParagraph.txt";
-            Setup(testCaseFile);
-
-            var parsedText = _markdownParser.Parse(_textToParse);
-
-            _expectedOutput.AddDocumentElement(_multiLineParagraph);
-            var equal = _expectedOutput.Equals(parsedText);
-            Assert.True(equal);
-        }
-
-        [Fact]
-        public void SectionWithNestedSubsections()
-        {
-            const string testCaseFile = "SectionWithNestedSubsections.txt";
-            Setup(testCaseFile);
-
-            var parsedText = _markdownParser.Parse(_textToParse);
-
-            _expectedOutput.AddDocumentElement(_section1);
-            _section1.AddDocumentElement(_oneLineParagraph);
-            _section1.AddDocumentElement(_subSection1);
-            _subSection1.AddDocumentElement(_subSectionParagraph);
-            _expectedOutput.AddDocumentElement(_section2);
-            _section2.AddDocumentElement(_section2Paragraph1);
-            var equal = _expectedOutput.Equals(parsedText);
-            Assert.True(equal);
-        }
-
-        [Fact]
-        public void OnlySubsection()
-        {
-            const string testCaseFile = "OnlySubsection.txt";
-            Setup(testCaseFile);
-
-            var parsedText = _markdownParser.Parse(_textToParse);
-
-            _expectedOutput.AddDocumentElement(_subSection1);
-            _subSection1.AddDocumentElement(_subSectionParagraph);
-            var equal = _expectedOutput.Equals(parsedText);
-            Assert.True(equal);
-        }
-
-        [Fact]
-        public void EmptyFile()
+        public void ParseEmptyFileTest()
         {
             const string testCaseFile = "EmptyFile.txt";
             Setup(testCaseFile);
 
             var parsedText = _markdownParser.Parse(_textToParse);
 
+            var equal = _expectedOutput.Equals(parsedText);
+            Assert.True(equal);
+        }
+
+        [Fact]
+        public void ParseEmptySectionTest()
+        {
+            const string testCaseFile = "EmptySection.txt";
+            Setup(testCaseFile);
+
+            var parsedText = _markdownParser.Parse(_textToParse);
+
+            _expectedOutput.AddDocumentElement(_section);
+            var equal = _expectedOutput.Equals(parsedText);
+            Assert.True(equal);
+        }
+
+        [Fact]
+        public void ParseOnlySubsectionTest()
+        {
+            const string testCaseFile = "OnlySubsection.txt";
+            Setup(testCaseFile);
+
+            var parsedText = _markdownParser.Parse(_textToParse);
+
+            _expectedOutput.AddDocumentElement(_subSection);
+            var equal = _expectedOutput.Equals(parsedText);
+            Assert.True(equal);
+        }
+
+        [Fact]
+        public void ParseEmptySectionAndSubSectionTest()
+        {
+            const string testCaseFile = "EmptySectionAndSubSection.txt";
+            Setup(testCaseFile);
+
+            var parsedText = _markdownParser.Parse(_textToParse);
+
+            _expectedOutput.AddDocumentElement(_section);
+            _section.AddDocumentElement(_subSection);
+            var equal = _expectedOutput.Equals(parsedText);
+            Assert.True(equal);
+        }
+
+        [Fact]
+        public void ParseParagraphWithTextAndBoldTextTest()
+        {
+            const string testCaseFile = "ParagraphWithTextAndBoldText.txt";
+            Setup(testCaseFile);
+
+            var parsedText = _markdownParser.Parse(_textToParse);
+
+            _expectedOutput.AddDocumentElement(_paragraph);
+            _paragraph.AddDocumentElement(_text);
+            _paragraph.AddDocumentElement(_boldText);
+            var equal = _expectedOutput.Equals(parsedText);
+            Assert.True(equal);
+        }
+
+        [Fact]
+        public void ParseMultilineParagraphTest()
+        {
+            const string testCaseFile = "MultilineParagraph.txt";
+            Setup(testCaseFile);
+
+            var parsedText = _markdownParser.Parse(_textToParse);
+
+            _expectedOutput.AddDocumentElement(_paragraph);
+            _paragraph.AddDocumentElement(_text);
+            _paragraph.AddDocumentElement(_text);
+            var equal = _expectedOutput.Equals(parsedText);
+            Assert.True(equal);
+        }
+
+        [Fact]
+        public void ParseOneSectionAndOneLineParagraphTest()
+        {
+            const string testCaseFile = "OneSectionAndOneLineParagraph.txt";
+            Setup(testCaseFile);
+
+            var parsedText = _markdownParser.Parse(_textToParse);
+
+            _expectedOutput.AddDocumentElement(_section);
+            _section.AddDocumentElement(_paragraph);
+            _paragraph.AddDocumentElement(_text);
+            var equal = _expectedOutput.Equals(parsedText);
+            Assert.True(equal);
+        }
+
+        [Fact]
+        public void ParseSectionWithNestedSubsectionsTest()
+        {
+            const string testCaseFile = "SectionWithNestedSubsections.txt";
+            Setup(testCaseFile);
+
+            var parsedText = _markdownParser.Parse(_textToParse);
+
+            _expectedOutput.AddDocumentElement(_section);
+            _section.AddDocumentElement(_subSection);
+            _subSection.AddDocumentElement(_nestedSubsection);
             var equal = _expectedOutput.Equals(parsedText);
             Assert.True(equal);
         }
